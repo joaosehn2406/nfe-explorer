269c09cdb2846bf7d755c3cf5e0e2d09fca58f8d
joao pedro <jpssantosos5874@gmail.com>
2026-04-02 00:00:33 -0300

    Adjusted components to implement the API response

--- frontend/src/app/features/import/import.component.ts
+++ frontend/src/app/features/import/import.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ImportCardComponent } from './import-card/import.card.component';
+import { NfeService } from '../../services/nfe.service';
+import { ImportNfeResponse } from '../../models/response/import.nfe.response';
 
 @Component({
   selector: 'app-import',
@@ -7,8 +9,28 @@ import { ImportCardComponent } from './import-card/import.card.component';
     ImportCardComponent
   ],
   templateUrl: './import.component.html',
-  styleUrl: './import.component.css',
+  styleUrl: './import.component.css'
 })
 export class ImportComponent {
 
+  importResult: ImportNfeResponse | null = null
+  errorMessage: Error | null = null
+
+  constructor(private nfeService: NfeService) {
+  }
+
+  onXmlSubmitted(payload: string | File) {
+    const request$ = payload instanceof File
+      ? this.nfeService.importByFile(payload)
+      : this.nfeService.importByText(payload);
+
+    request$.subscribe({
+      next: (result) => {
+        this.importResult = result
+      },
+      error: (err: Error) => {
+        this.errorMessage = err
+      }
+    })
+  }
 }
