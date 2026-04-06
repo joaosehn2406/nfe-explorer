06c4133ba1c83eeedbf932f6e44ce51aa4c95cd7
joao.pedro <jpssantosos5874@gmail.com>
2026-04-06 20:47:06 -0300

    Implemented loading bar

--- frontend/src/app/features/import/import.component.ts
+++ frontend/src/app/features/import/import.component.ts
@@ -1,14 +1,13 @@
-import {Component, signal} from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ImportCardComponent } from './import-card/import.card.component';
-import { NfeService } from '../../services/nfe.service';
+import { NfeService, UploadProgress } from '../../services/nfe.service';
 import { ImportNfeResponse } from '../../models/response/import.nfe.response';
 import { ApiErrorResponse } from '../../models/response/api.error.response';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-import',
-  imports: [
-    ImportCardComponent
-  ],
+  imports: [ImportCardComponent, MatProgressBarModule],
   templateUrl: './import.component.html',
   styleUrl: './import.component.css'
 })
@@ -16,24 +15,30 @@ export class ImportComponent {
 
   importResult = signal<ImportNfeResponse | null>(null);
   errorMessage = signal<string | null>(null);
+  isLoading = signal<boolean>(false);
+  uploadProgress = signal<number>(0);
 
   constructor(private nfeService: NfeService) {
   }
 
   onXmlSubmitted(payload: string | File) {
-    this.errorMessage.set(null)
+    this.errorMessage.set(null);
+    this.importResult.set(null);
+    this.uploadProgress.set(0);
+    this.isLoading.set(true);
 
-    const request$ = payload instanceof File
-      ? this.nfeService.importByFile(payload)
-      : this.nfeService.importByText(payload);
-
-    request$.subscribe({
-      next: (result) => {
-        this.importResult.set(result)
+    this.nfeService.importNfe(payload).subscribe({
+      next: ({ progress, result }: UploadProgress) => {
+        this.uploadProgress.set(progress);
+        if (result) {
+          this.importResult.set(result);
+          this.isLoading.set(false);
+        }
       },
       error: (err: ApiErrorResponse) => {
-        this.errorMessage.set(err.message)
+        this.errorMessage.set(err.message);
+        this.isLoading.set(false);
       }
-    })
+    });
   }
 }
