f8a003c3a46702b0bffd6e8d9f35d96e149d8ecf
joao.pedro <jpssantosos5874@gmail.com>
2026-07-09 13:36:00 -0300

    refactor: apply HTTPS redirection only in production

--- backend/NfeExplorer-Api/NfeExplorer-Api/Program.cs
+++ backend/NfeExplorer-Api/NfeExplorer-Api/Program.cs
@@ -36,8 +36,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHttpsRedirection();
+}
 
-app.UseHttpsRedirection();
 app.UseCors();
 app.MapControllers();
 app.Run();
