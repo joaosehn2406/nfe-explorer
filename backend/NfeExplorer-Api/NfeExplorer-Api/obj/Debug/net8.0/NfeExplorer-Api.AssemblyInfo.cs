cb0867891baad7d3c2793334af7d59003551b812
joaopedro <jpssantosos5874@gmail.com>
2026-08-15 00:26:28 -0300

    fix: renamed pt-BR routes

--- backend/NfeExplorer-Api/NfeExplorer-Api/obj/Debug/net8.0/NfeExplorer-Api.AssemblyInfo.cs
+++ backend/NfeExplorer-Api/NfeExplorer-Api/obj/Debug/net8.0/NfeExplorer-Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NfeExplorer-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4282bf17bd84234b18a25e10ab2548994980905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea938e05ef86c4180fe42cf45b27e35ef746bcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("NfeExplorer-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NfeExplorer-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
